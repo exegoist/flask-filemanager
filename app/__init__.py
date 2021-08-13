fb08a2079bc70267088e493811dddf078ebc33d3
exegoist <exegoist@icloud.com>
2021-08-13 12:31:19 +0300

    fixed search + pagination, added session

--- app/__init__.py
+++ app/__init__.py
@@ -2,6 +2,8 @@ from flask import Flask
 from pony.orm import *
 
 app = Flask(__name__)
+app.config["SECRET_KEY"] = "GHhw8sjj29GHts[pkq"
+
 db = Database()
 db.bind(provider='sqlite', filename='../database.sqlite', create_db=True)
 
--- app/routes.py
+++ app/routes.py
@@ -3,7 +3,7 @@ from datetime import datetime
 
 from app.models import File
 from app import app, db_session, desc
-from flask import render_template, redirect, url_for, request
+from flask import render_template, redirect, url_for, request, session
 from config import DIR, PAGESIZE
 
 
@@ -11,23 +11,48 @@ from config import DIR, PAGESIZE
 @app.route('/<int:page>')
 @db_session
 def index(page=1):
-    q = request.args.get('search')
+    p = request.args.get('page')
+    if p:
+        page = int(p)
+    if 'q' in session:
+        req = request.args.get('search')
+        if req:
+            q = req
+            page = 1
+            session['q'] = req
+        elif req == '':
+            q = ''
+        else:
+            q = session['q']
+    else:
+        q = request.args.get('search')
+        if q:
+            page = 1
+            session['q'] = q
     if q:
         files = File.select(lambda p: q in p.name).order_by(desc(File.time)).page(page, pagesize=PAGESIZE)
         col = File.select(lambda p: q in p.name).count()
         if col % PAGESIZE == 0:
-            pages = [i + 1 for i in range(col // PAGESIZE)]
+            pages = [i + 1 for i in range(col // PAGESIZE)][:20]
+            total = col // PAGESIZE
         else:
-            pages =  [i + 1 for i in range(col // PAGESIZE + 1)]
+            pages =  [i + 1 for i in range(col // PAGESIZE + 1)][:20]
+            total = col // PAGESIZE + 1
     else:
         files = File.select().order_by(desc(File.time)).page(page, pagesize=PAGESIZE)
         col = File.select().count()
         if col % PAGESIZE == 0:
-            pages = [i + 1 for i in range(col // PAGESIZE)]
+            pages = [i + 1 for i in range(col // PAGESIZE)][:20]
+            total = col // PAGESIZE
         else:
-            pages =  [i + 1 for i in range(col // PAGESIZE + 1)]
-    return render_template('index.html', files=files, pages=pages)
+            pages =  [i + 1 for i in range(col // PAGESIZE + 1)][:20]
+            total = col // PAGESIZE + 1
+    return render_template('index.html', files=files, pages=pages, total=total)
 
+@app.route('/clear')
+def clear_session():
+    session.pop('q', None)
+    return redirect(url_for('index'))
 
 @app.route('/scan')
 @db_session
@@ -52,4 +77,4 @@ def delete_file(del_num):
         file.delete()
         return redirect(url_for('index'))
     except:
-        return "Some trouble with deleting"
+        return "<h1>Some trouble with deleting file</h1>"
